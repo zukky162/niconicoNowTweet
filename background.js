9ac50ff1c5e1a797cfa60333fc228ed592e56255
zukky162 <zukky162@gmail.com>
2015-06-11 21:58:40 +0900

    update trimming method

--- background.js
+++ background.js
@@ -1,18 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-/*
-    var dx, dy;
-    chrome.tabs.sendMessage(tab.id, {command: "prepare"}, function(msg) {
-        dx = msg.dx;
-        dy = msg.dy;
-    });
-*/
-
     chrome.tabs.captureVisibleTab(null, {"format":"png"}, function(dataUrl) {
-/*
-        chrome.tabs.sendMessage(tab.id,
-                                {command: "undo", dx: dx, dy: dy},
-                                null);
-*/
         chrome.tabs.sendMessage(tab.id,
                                 {command: "trimming", dataUrl: dataUrl},
                                 function(msg) {
@@ -20,7 +7,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
             window.open(msg);
             var a = document.createElement("a");
             a.href = msg;
-            a.setAttribute("download", name || "nicovideo.png");
+            a.setAttribute("download", "nicovideo.png");
             a.dispatchEvent(new CustomEvent("click"));
         });
     });
--- content.js
+++ content.js
@@ -1,33 +1,41 @@
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-/*
-    if(msg.command == "prepare") {
-        var header = document.getElementById("siteHeader");
-        header.style.display = "none";
-
-        var player = document.getElementById("nicoplayerContainer");
-        var rect   = player.getBoundingClientRect();
-        var dx = rect.left;
-        var dy = rect.top;
-        window.scrollBy(dx, dy);
-
-        sendResponse({dx: dx, dy: dy});
-        return;
-    }
-*/
-
     if(msg.command == "trimming") {
+        // プレーヤー
         var player = document.getElementById("nicoplayerContainer");
-        if(!player) {
-            return;
-        }
+        // 現在のページにプレーヤーが存在しない
+        if(!player) return;
+        // プレーヤーの矩形
         var rect   = player.getBoundingClientRect();
-
+        // 動画の下についてるコントロールパネルの高さ。
+        // 本当は自動で取得したいけどやりかたわからぬ。
+        var cPanelH = 77;
+        // 中画面の場合はcPanelHを83に設定。こうしないと余白除去がずれる。
+        var clist = document.body.classList;
+        if(clist && clist.contains("size_medium")) cPanelH = 83;
+        // 1ピクセルを何倍で表示しているか（＝拡大率）
         var zoomer = window.devicePixelRatio;
-
-        var x      = Math.round(rect.left   * zoomer);
-        var y      = Math.round(rect.top    * zoomer);
-        var width  = Math.round(rect.width  * zoomer);
-        var height = Math.round(rect.height * zoomer);
+        // スクショから切り抜く部分を決める
+        var x      = rect.left   * zoomer;
+        var y      = rect.top    * zoomer;
+        var width  = rect.width  * zoomer;
+        var height = rect.height * zoomer - cPanelH;
+        // 余白除去の処理
+        if(height * 16 <= width * 9) {
+            // ぴったり、または横に余白有り
+            var offsetX = (width * 9 - height * 16) / 18.0;
+            x += offsetX;
+            width -= offsetX * 2;
+        } else {
+            // 縦に余白有り
+            var offsetY = (height * 16 - width * 9) / 32.0;
+            y += offsetY;
+            height -= offsetY * 2;
+        }
+        // 実数なので四捨五入
+        x      = Math.round(x);
+        y      = Math.round(y);
+        width  = Math.round(width);
+        height = Math.round(height);
 
         var canvas = document.createElement('canvas');
         canvas.width  = width;
@@ -46,12 +54,4 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
         sendResponse(canvas.toDataURL());
         return;
     }
-/*
-    if(msg.command == "undo") {
-        var header = document.getElementById("siteHeader");
-        header.style.display = "";
-        window.scrollBy(-msg.dx, -msg.dy);
-        return;
-    }
-*/
 });
